fbc70e323a1b950a010990e9a392fbb69da39426
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-04-03 14:56:05 -0700

    0

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+if __name__ == '__main__':
+    pass
