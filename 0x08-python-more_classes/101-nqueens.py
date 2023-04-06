1b3b1e29d9c7f0888e711c666c73233342b0b236
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-04-06 14:47:32 -0700

    0

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,35 +1,48 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import sys
 
-def nqueens(N):
-    if not isinstance(N, int):
-        print("N must be a number")
-        sys.exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    
-    def solve(queens, xy_sum, xy_diff):
-        row = len(queens)
-        if row == N:
-            print(queens)
+
+def check_two_queens(new_queen_position, other_queen_position):
+    """Check if two queens are attacking each other."""
+    if new_queen_position[0] == other_queen_position[0] or new_queen_position[1] == other_queen_position[1]:
+        return True
+    elif abs(new_queen_position[0] - other_queen_position[0]) == abs(new_queen_position[1] - other_queen_position[1]):
+        return True
+    else:
+        return False
+
+
+def solve_nqueens(n):
+    """Solve the N queens puzzle."""
+    solutions = []
+
+    def backtrack(queens):
+        if len(queens) == n:
+            solutions.append(queens)
             return
-        
-        for col in range(N):
-            if col not in queens and row+col not in xy_sum and row-col not in xy_diff:
-                solve(queens + [col], xy_sum + [row+col], xy_diff + [row-col])
-    
-    solve([], [], [])
+        row = len(queens)
+        for col in range(n):
+            if all(not check_two_queens((row, col), (r, c)) for r, c in queens):
+                backtrack(queens + [(row, col)])
+
+    backtrack([])
+    return solutions
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
-    nqueens(N)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for s in solutions:
+        print(' '.join(f"{r + 1}" for r, c in s))
