06622b50cfa2a159a4e1a8a05a63c98183bf61e8
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-04-06 15:06:51 -0700

    0

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,51 +1,39 @@
 #!/usr/bin/python3
-import sys
+def print_board(board):
+    for row in board:
+        print([i for i, x in enumerate(row) if x == 1])
 
+def is_safe(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
 
-class NQueens:
-    def __init__(self, n):
-        self.n = n
-        self.columns = []
-        self.solutions = []
+def solve_nqueens(board, col, n, solutions):
+    if col == n:
+        solutions.append([i for i, row in enumerate(board) if 1 in row])
+        return
+    for row in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            solve_nqueens(board, col + 1, n, solutions)
+            board[row][col] = 0
 
-    def solve(self):
-        self.place_queen(0)
-        self.print_solutions()
-
-    def place_queen(self, row):
-        if row == self.n:
-            self.solutions.append(list(self.columns))
-        else:
-            for col in range(self.n):
-                if self.is_valid(row, col):
-                    self.columns.append(col)
-                    self.place_queen(row+1)
-                    self.columns.pop()
-
-    def is_valid(self, row, col):
-        for r, c in enumerate(self.columns):
-            if c == col or r-c == row-col or r+c == row+col:
-                return False
-        return True
-
-    def print_solutions(self):
-        for solution in self.solutions:
-            print(solution)
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
+def nqueens(n):
+    if not isinstance(n, int):
         print("N must be a number")
-        sys.exit(1)
-
+        exit(1)
     if n < 4:
         print("N must be at least 4")
-        sys.exit(1)
-
-    solver = NQueens(n)
-    solver.solve()
+        exit(1)
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+    solve_nqueens(board, 0, n, solutions)
+    for solution in solutions:
+        print(solution)
