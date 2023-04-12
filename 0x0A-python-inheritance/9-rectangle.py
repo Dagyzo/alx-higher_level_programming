b678cd8af75260636d22cb4cb005894909cb18c4
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-04-11 17:50:52 -0700

    inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,29 +1,50 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = _import_('7-base_geometry').BaseGeometry
+"""
+Defines a rectangle class that inherits from BaseGeometry
+"""
 
 
-class Rectangle(Basegeometry):
-    """REpresents a rectangle using BaseGeometry."""
+class BaseGeometry:
+    """BaseGeometry class"""
 
-    def _init_(self, width, height):
-        """Initialize a new Rectangle.
+    def area(self):
+        """Raises an Exception with an explanation message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a given value if it is a positive integer
 
         Args:
-            width (int): The width of the new Rectangle.
-            Height (int): The height of the new Rectangle.
+            name (str): the name string
+            value (int): the value to validate
+
+        Raises:
+            TypeError: if `value` is not an integer
+            ValueError: if `value` is less or equal to 0
         """
-        super().integer_validator("width", width)
-        self._width = width
-        super().integer_validator("height", height)
-        self._height = height
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
 
-    def area()self:
-        """Return the area of the rectangle."""
-        return self._width * self._height
 
-    def _str_(self):
-        """Return the print() and str() representation of a rectangle."""
-        string = "[" + str(self._classs_._name_) + "]"
-        string += str(self._width) + "/" + str(self._height)
-        return string
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle instance
+
+        Args:
+            width (int): width of the rectangle, must be a positive integer
+            height (int): height of the rectangle, must be a positive integer
+        """
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation of the rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
