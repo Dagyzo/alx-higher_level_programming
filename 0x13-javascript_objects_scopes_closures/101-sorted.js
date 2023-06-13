f787b5641baf319e4e56e539f023acd7beb40583
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-06-13 10:34:14 -0700

    java objects

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-const newDiction = {};
-
-Object.keys(dict).map(function (key, index) {
-  if (newDiction[dict[key]] === undefined) {
-    newDiction[dict[key]] = [];
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
-  newDiction[dict[key]].push(key);
-});
-
-console.log(newDiction);
+  newDict[dict[key]].push(key);
+}
+console.log(newDict);
