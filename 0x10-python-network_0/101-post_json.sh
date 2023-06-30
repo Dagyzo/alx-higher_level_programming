5b1a0bc29318272e8b6eb00305182ccac40ca885
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-06-30 14:36:59 -0700

    push

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-url=$1
-filename=$2
-
-# Read the contents of the file into a variable
-file_content=$(cat "$filename")
-
-# Send the POST request with curl and display the response body
-response=$(curl -X POST -H "Content-Type: application/json" -d "$file_content" "$url")
-echo "$response"
+# sends a JSON POST request to a URL, and displays the body of the response.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
