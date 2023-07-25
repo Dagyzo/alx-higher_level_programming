8a42429afad00b8040be3c3ca86806e75727be10
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-07-25 13:06:00 -0700

    java

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,26 @@
-#!/usr/bin/python3
-import requests
-
-def count_completed_tasks(api_url):
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        todos = response.json()
-        completed_tasks = {}
-        for todo in todos:
-            if todo['completed']:
-                user_id = todo['userId']
-                completed_tasks[user_id] = completed_tasks.get(user_id, 0) + 1
-
-        for user_id, count in completed_tasks.items():
-            print(f"User ID: {user_id}, Completed Tasks: {count}")
-    else:
-        print(f"Error: Failed to retrieve data from {api_url}")
-
-# Main script
-api_url = "https://jsonplaceholder.typicode.com/todos"
-count_completed_tasks(api_url)
+#!/usr/bin/node
+/**
+  compute number of tasks completed by user id
+  usage: /6-completed_tasks.js <API URL>
+  */
+const myArgs = process.argv.slice(2);
+const request = require('request');
+const id_ = [];
+const result = {};
+request(myArgs[0], function (error, response, body) {
+  let i;
+  if (!error) {
+    const json_ = JSON.parse(body);
+    const len = json_.length;
+    for (i = 0; i < len; i++) {
+      if (json_[i].completed) {
+        id_.push(json_[i].userId);
+      }
+    }
+    for (let j = 0; j < id_.length; ++j) {
+      if (!result[id_[j]]) { result[id_[j]] = 0; }
+      ++result[id_[j]];
+    }
+  }
+  console.log(result);
+});
