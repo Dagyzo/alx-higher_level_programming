6e3af8a21dfe7cd9b68e2b4fefe1c3dfd8f65e0f
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-07-25 05:03:05 -0700

    js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,27 +1,16 @@
 #!/usr/bin/node
-const fetch = require('node-fetch');
-
-async function getCompletedTasksCount(apiUrl) {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    let userCompletedTasks = {};
-
-    data.forEach(task => {
-        if (task.completed) {
-            if (Object.prototype.hasOwnProperty.call(userCompletedTasks, task.userId)) {
-                userCompletedTasks[task.userId]++;
-            } else {
-                userCompletedTasks[task.userId] = 1;
-            }
-        }
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
     });
-
-    for (let userId in userCompletedTasks) {
-        if (Object.prototype.hasOwnProperty.call(userCompletedTasks, userId)) {
-            console.log(`User ID ${userId} has completed ${userCompletedTasks[userId]} tasks`);
-        }
-    }
-}
-
-getCompletedTasksCount('https://jsonplaceholder.typicode.com/todos');
-
+    console.log(completed);
+  }
+});
