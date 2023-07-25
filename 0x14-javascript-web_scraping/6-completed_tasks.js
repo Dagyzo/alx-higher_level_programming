9567ae67e46b6b02b57fefbdccaab561da3d413f
Douglas Nyakundi <douglasmigiro@gmail.com>
2023-07-25 13:17:00 -0700

    java

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,31 +1,26 @@
 #!/usr/bin/node
-
+/**
+  compute number of tasks completed by user id
+  usage: /6-completed_tasks.js <API URL>
+  */
+const myArgs = process.argv.slice(2);
 const request = require('request');
-
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  const todos = JSON.parse(body);
-  const completedByUser = {};
-
-  todos.forEach((todo) => {
-    if (todo.completed) {
-      const userId = todo.userId;
-      
-      if (completedByUser[userId]) {
-        completedByUser[userId] += 1;
-      } else {
-        completedByUser[userId] = 1; 
+const id_ = [];
+const result = {};
+request(myArgs[0], function (error, response, body) {
+  let i;
+  if (!error) {
+    const json_ = JSON.parse(body);
+    const len = json_.length;
+    for (i = 0; i < len; i++) {
+      if (json_[i].completed) {
+        id_.push(json_[i].userId);
       }
     }
-  });
-
-  Object.keys(completedByUser).forEach((userId) => {
-    console.log(`User ${userId}: ${completedByUser[userId]}`);
-  });
+    for (let j = 0; j < id_.length; ++j) {
+      if (!result[id_[j]]) { result[id_[j]] = 0; }
+      ++result[id_[j]];
+    }
+  }
+  console.log(result);
 });
